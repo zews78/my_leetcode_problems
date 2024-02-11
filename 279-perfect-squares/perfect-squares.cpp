63d9fef8f58ebf487f5d0193e82a733f166fab50
Sharique Alam <56071706+zews78@users.noreply.github.com>
2024-02-12 00:43:38 +0530

    Time: 286 ms (9.55%) | Memory: 198 MB (5.25%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,45 +1,49 @@
 class Solution {
 public:
 
-    // int solve(int n, int p, vector<int> &perfectSq){
-    //     //bc
-    //     if(n==0){
-    //         return 0;
-    //     }
-    //     if(p==0){
-    //         return 0;
-    //     }
+    int solve(int n, int p, vector<int> &perfectSq, vector<vector<int>> &dp){
+        //bc
+        if(n==0){
+            return 0;
+        }
+        if(p==0){
+            return INT_MAX;
+        }
+        if(dp[n][p]!=-1){
+            return dp[n][p];
+        }
 
-    //     if(n<perfectSq[p-1]){
-    //         return solve(n, p-1, perfectSq);
-    //     }
-    //     else{
-    //         return min(solve(n, p-1, perfectSq), 1+solve(n-perfectSq[p-1], p, perfectSq));
-    //     }
+        if(n<perfectSq[p-1]){
+            return dp[n][p] = solve(n, p-1, perfectSq,dp);
+        }
+        else{
+            return dp[n][p] = min(solve(n, p-1, perfectSq, dp), 1+solve(n-perfectSq[p-1], p, perfectSq, dp));
+        }
 
-    // }
+    }
 
     int numSquares(int n) {
-        // vector<int> perfectSq;
-        // int i=1;
-        // while(i*i<=n){
-        //     perfectSq.push_back(i*i);
-        //     i++;
-        // }
-        // int p= perfectSq.size();
-
-        // int res = solve(n, p, perfectSq);
-        // return res;
-        // return p;
-        vector<int> dp(n+1, INT_MAX);
-        dp[0]=0;
-        for(int i=1; i<=n; i++){
-            for(int j=1; j*j<=i; j++){
-                dp[i] = min(dp[i], dp[i-j*j]+1);
-            }
-
+        vector<int> perfectSq;
+        int i=1;
+        while(i*i<=n){
+            perfectSq.push_back(i*i);
+            i++;
         }
-        return dp[n];
+        int p= perfectSq.size();
+        vector<vector<int>> dp(n+1, vector<int>(p+1, -1));
+
+        int res = solve(n, p, perfectSq, dp);
+        return res;
+        // return p;
+        // vector<int> dp(n+1, INT_MAX);
+        // dp[0]=0;
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j*j<=i; j++){
+        //         dp[i] = min(dp[i], dp[i-j*j]+1);
+        //     }
+
+        // }
+        // return dp[n];
 
     }
 };
