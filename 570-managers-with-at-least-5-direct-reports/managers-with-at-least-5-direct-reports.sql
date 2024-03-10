dc9994152a50988b0848db2c7fcbe5fa1b1a756b
Sharique Alam <56071706+zews78@users.noreply.github.com>
2024-03-11 03:47:56 +0530

    Time: 1345 ms (68.98%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,18 +1,16 @@
 /* Write your T-SQL query statement below */
--- select a.name, count(b.id)
--- -- select *
--- from employee a, employee b
--- -- cross join employee b
-
--- where a.id <> b.managerId
--- group by a.managerid, a.name
--- -- having count(b.managerId)>=25
+-- select *
+select a.name
+from employee a
+inner join employee b on a.id=b.managerId
+group by b.managerid, a.name 
+having count(b.managerId)>4
 
 -- select id, count(managerID)
 -- from employee
 -- group by id
 
-select name
-from employee
-where id in (select managerId from employee group by managerid having count(managerId)>4)
+-- select name
+-- from employee
+-- where id in (select managerId from employee group by managerid having count(managerId)>4)
 
