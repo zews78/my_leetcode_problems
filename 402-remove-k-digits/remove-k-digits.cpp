1bc07a78e8a5fecdc9f43c92879afea19982c252
Sharique Alam <56071706+zews78@users.noreply.github.com>
2024-04-12 16:26:36 +0530

    Time: 11 ms (59.47%) | Memory: 10.4 MB (18.48%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,29 +1,34 @@
 class Solution {
- public:
-  string removeKdigits(string num, int k) {
-    if (num.length() == k)
-      return "0";
-
-    string ans;
-    vector<char> stack;
-
-    for (int i = 0; i < num.length(); ++i) {
-      while (k > 0 && !stack.empty() && stack.back() > num[i]) {
-        stack.pop_back();
-        --k;
-      }
-      stack.push_back(num[i]);
+public:
+    std::string removeKdigits(std::string num, int k) {
+        std::stack<char> stack;
+        
+        for (char digit : num) {
+            while (!stack.empty() && k > 0 && stack.top() > digit) {
+                stack.pop();
+                k--;
+            }
+            stack.push(digit);
+        }
+        
+        // Remove remaining k digits from the end of the stack
+        while (k > 0 && !stack.empty()) {
+            stack.pop();
+            k--;
+        }
+        
+        // Construct the resulting string from the stack
+        std::string result;
+        while (!stack.empty()) {
+            result += stack.top();
+            stack.pop();
+        }
+        std::reverse(result.begin(), result.end()); // Reverse to get the correct order
+        
+        // Remove leading zeros
+        size_t pos = result.find_first_not_of('0');
+        result = (pos == std::string::npos) ? "0" : result.substr(pos);
+        
+        return result;
     }
-
-    while (k-- > 0)
-      stack.pop_back();
-
-    for (const char c : stack) {
-      if (c == '0' && ans.empty())
-        continue;
-      ans += c;
-    }
-
-    return ans.empty() ? "0" : ans;
-  }
 };
