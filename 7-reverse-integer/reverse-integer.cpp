52097c2e2691046af4cc5729704ade1083b46542
Sharique Alam <56071706+zews78@users.noreply.github.com>
2024-10-10 15:27:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (8.22%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -2,23 +2,27 @@ class Solution {
 public:
     int reverse(int x) {
         long long int ans=0;
+        int x2 = x;
         int n=0;//size of x
-        vector<int> arr;
+        // vector<int> arr;
         while(x){
-            arr.push_back(x%10);
+            // arr.push_back(x%10);
             // cout<<x%10<<endl;
             x = x/10;
             n++;
         }
         // cout<<n;
         for(int i=0; i<n; i++){
-            ans += pow(10,(n-i-1))* arr[i];
+            ans += pow(10,(n-i-1))* (x2%10);
+            x2 /= 10;
             // cout<<ans<<endl;
         }
         if(ans > INT_MAX || ans<INT_MIN){
             return 0;
         }
         return ans;
+
+        // while()
         
     }
 };
