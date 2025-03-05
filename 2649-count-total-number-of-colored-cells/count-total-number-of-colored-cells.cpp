4874c24402b0b4e8ac41a9148fac0296241a9d16
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-03-05 06:11:06 +0530

    Time: 3 ms (20.68%) | Memory: 8 MB (31.17%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long out = n;
-        return 2*out*out -2*out +1;
+        long long result= 1;
+        while(n){
+            n--;
+            result= result + 4*n;
+        }
+        return result;
+
+        // long long out = n;
+        // return (long long)2*n*n -2*n +1;
         // return result;
     }
 };
