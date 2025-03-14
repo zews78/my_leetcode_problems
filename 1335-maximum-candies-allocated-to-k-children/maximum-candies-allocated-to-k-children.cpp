7488bb802b8395328f9b4bb82150fea3d00b0231
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-03-14 23:17:26 +0530

    Time: 42 ms (22.92%) | Memory: 145.5 MB (5.02%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -1,49 +1,71 @@
 class Solution {
 public:
-    int maximumCandies(vector<int>& candies, long long k) {
-        // Find the maximum number of candies in any pile
-        int maxCandiesInPile = 0;
-        for (int i = 0; i < candies.size(); i++) {
-            maxCandiesInPile = max(maxCandiesInPile, candies[i]);
+    bool check_feasible_dist(vector<int> temp_candies, long long temp_k, int max_number, int n){
+        if(max_number==0) return false;
+        for(int i=0; i<n; i++){
+            temp_k -= temp_candies[i]/max_number;
         }
-
-        // Set the initial search range for binary search
-        int left = 0;
-        int right = maxCandiesInPile;
-
-        // Binary search to find the maximum number of candies each child can
-        // get
-        while (left < right) {
-            // Calculate the middle value of the current range
-            int middle = (left + right + 1) / 2;
-
-            // Check if it's possible to allocate candies so that each child
-            // gets 'middle' candies
-            if (canAllocateCandies(candies, k, middle)) {
-                // If possible, move to the upper half to search for a larger
-                // number
-                left = middle;
-            } else {
-                // Otherwise, move to the lower half
-                right = middle - 1;
+        if(temp_k <=0){
+            return true;
+        }
+        return false;
+            // int i=0;
+            // int cont_bounce=0;
+            // while(temp_k){
+            //     if(temp_candies[i%n]-max_number>=0){
+            //         temp_candies[i%n]-= max_number;
+            //         temp_k--;
+            //         cont_bounce=0;
+            //     }else{
+            //         cont_bounce++;
+            //     }
+            //     i++;
+            //     //breaking statement
+            //     if(cont_bounce==n){
+            //         return false;
+            //     }
+            // }
+            // return true;
+    }
+    int maximumCandies(vector<int>& candies, long long k) {
+        int n = candies.size();
+        long long total_sum=0;
+        int i=1;
+        int j=candies[0];
+        for(auto x: candies){
+            j = max(x, j);
+            total_sum+=x;
+        }
+        if(total_sum<k){
+            return 0;
+        }
+        // if(total_sum==k){
+        //     return 1;
+        // }
+        // cout<<j<<endl;
+        int ans = 0;
+        while(i<=j){
+            int mid = i +(j-i)/2;
+            if(check_feasible_dist(candies, k, mid, n)){
+                ans = mid;
+                i = mid+1;
+            }else{
+                j= mid-1;
             }
         }
+        // cout<<check_feasible_dist(candies, k, 4, n);
+        return ans;
 
-        return left;
-    }
+        // while(true){
+        //     if(!check_feasible_dist(candies, k, max_number, n)){
+        //         return max_number-1;
+        //     }else{
+        //         max_number++;
+        //     }
+        //     cout<<max_number<<" ";
 
-private:
-    bool canAllocateCandies(vector<int>& candies, long long k,
-                            int numOfCandies) {
-        // Initialize the total number of children that can be served
-        long long int maxNumOfChildren = 0;
-
-        for (int pileIndex = 0; pileIndex < candies.size(); pileIndex++) {
-            maxNumOfChildren +=
-                candies[pileIndex] /
-                numOfCandies;  // Add the number of children this pile can serve
-        }
-
-        return maxNumOfChildren >= k;
+        //     //breaking statement
+        // }
+        // return max_number-1;
     }
 };
