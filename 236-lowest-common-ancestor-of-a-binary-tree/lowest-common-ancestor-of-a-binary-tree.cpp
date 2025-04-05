b53271461b04c9cea8fd4476511ab61227f5cd7e
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-04-05 09:26:48 +0530

    Time: 10 ms (77.96%) | Memory: 17.3 MB (89.06%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,41 +9,25 @@
  */
 class Solution {
 public:
-bool getPath_to_target(TreeNode* root, TreeNode* p, vector<TreeNode*>& store_path) {
-    if (root == nullptr) return false;
-
-    store_path.push_back(root);
-
-    if (root == p) return true;
-
-    if (getPath_to_target(root->left, p, store_path) || getPath_to_target(root->right, p, store_path))
-        return true;
-
-    // Backtrack if not found in either subtree
-    store_path.pop_back();
-    return false;
-}
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> path_p, path_q;
-        getPath_to_target(root, p, path_p);
-        getPath_to_target(root, q, path_q);
+        if(root==NULL || root==p || root==q){
+            return root;
+        }
 
-        for(int i=0; i<path_p.size(); i++){
-            cout<<path_p[i]->val<<" ";
-        }cout<<endl;
-        for(int i=0; i<path_q.size(); i++){
-            cout<<path_q[i]->val<<" ";
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+
+        //return (left==NULL)? right: left;
+        if(left==NULL){
+            return right;
+        }else if(right == NULL){
+            return left;
+        }else{
+            return root;
         }
-        int min_h = min(path_p.size(), path_q.size());
-        TreeNode* prevPath;
-        for(int i=0; i<min_h; i++){
-            if(path_p[i]!=path_q[i]){
-                break;
-            }
-            prevPath = path_p[i];
-        }
-        return prevPath;
+
 
     }
 };
