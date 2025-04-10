28544dda5492054031a1939d5dde338a7b330d42
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-04-10 21:24:02 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.57%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int pivot;
         int n = nums.size();
-        int s=0, e=n-1;
-        if(nums[0]<nums[e]){
-            pivot =0;
-        }
+        int s = 0;
+        int e = n-1;
 
-        while(s<e){
-            int mid = s +(e-s)/2;
+        while(s<=e){
+            int mid = s+(e-s)/2;
 
-            if(nums[mid]>= nums[0]){
-                s = mid+1;
-            }else{
-                e = mid;
+            if(nums[mid] == target){
+                return mid;
             }
-        }
-        pivot = e;
-        cout<<pivot;
-
-        if(target>=nums[pivot] && target<=nums[n-1]){
-            int s = pivot, e = n-1;
-            while(s<=e){
-                int mid = s+(e-s)/2;
-                if(nums[mid]==target){
-                    return mid;
-                }else if(nums[mid]< target){
-                    s = mid+1;
-                }else{
+            else if(nums[s]<=nums[mid]){
+                if(target<=nums[mid] && target>=nums[s]){
                     e = mid-1;
+                }else{
+                    s = mid+1;
                 }
-            }
-        }else{
-            int s= 0, e= pivot-1;
-            while(s<=e){
-                int mid = s+(e-s)/2;
-                if(nums[mid]==target){
-                    return mid;
-                }else if(nums[mid]< target){
+            }else{
+                if(target>=nums[mid] && target<=nums[e]){
                     s = mid+1;
                 }else{
                     e = mid-1;
