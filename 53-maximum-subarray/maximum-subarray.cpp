f1951014696c6994f0758beea046fbc5e2bb6627
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-04-11 09:47:48 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (81.33%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int max_ans = 0;
+        int max_ans = INT_MIN;
         int ans =0;
         //if all val -ve
-        bool allNeg =true;
-        int max_neg = INT_MIN;
-        for(auto x: nums){
-            if(x>=0){
-                allNeg= false;
-            }else{
-                max_neg = max(max_neg, x);
-            }
-        }
-        if(allNeg) return max_neg;
+        // bool allNeg =true;
+        // int max_neg = INT_MIN;
+        // for(auto x: nums){
+        //     if(x>=0){
+        //         allNeg= false;
+        //     }else{
+        //         max_neg = max(max_neg, x);
+        //     }
+        // }
+        // if(allNeg) return max_neg;
 
         for(auto x: nums){
             ans+=x;
+            max_ans = max(ans, max_ans);
+
             if(ans<0){
                 ans =0;
             }
-            max_ans = max(ans, max_ans);
         }
         return max_ans;
     }
