b8f2180296b6eef00dbbb88353d45656d3faddf4
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-04-19 13:45:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (96.54%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
-    int cal_nCr(int n, int r){
-        long long result =1;
-        for(int i=1; i<=r; i++){
-            result *= (n-r+i);
-            result/=i;
-        }
-        return result;
-        
-    }
+    // int solve(int m, int n){
+    //     if(m==0 && n==0){
+    //         return 1;
+    //     }else if(m<0 || n<0){
+    //         return 0;
+    //     }
+    //     else{
+    //         return solve(m-1, n) + solve(m, n-1);
+    //     }
+    // }
     int uniquePaths(int m, int n) {
-        return cal_nCr(m+n-2, n-1);
+        // return solve(m-1, n-1);
+
+        int dp[m+1][n+1];
+        for(int i=0; i<=m; i++){
+            for(int j=0; j<=n; j++){
+                if(i==1 && j==1){
+                    dp[i][j]=1;
+                }else if(i==0 || j==0){
+                    dp[i][j] = 0;
+                }else{
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        return dp[m][n];
+
+
     }
 };
