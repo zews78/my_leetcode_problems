ae7c923fd5db19fbc6bd821dccb48fb5f82b2146
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-04-23 18:31:20 +0530

    Time: 3 ms (89.83%) | Memory: 23.8 MB (59.41%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
         vector<vector<int>> output;
         sort(intervals.begin(), intervals.end());
 
         int left = intervals[0][0];
-        int right =intervals[0][1];
-        for(int i=1; i<intervals.size(); i++){
-            int current_left = intervals[i][0];
-            int current_right = intervals[i][1];
-            
-            if(current_left<= right){
-                right = max(right, current_right);
+        int right = intervals[0][1];
+
+        for(int i=1; i<n; i++){
+            int curr_left= intervals[i][0];
+            int curr_right= intervals[i][1];
+            if(curr_left<=right){
+                right = max(curr_right, right);
             }else{
                 output.push_back({left, right});
-                left = current_left;
-                right = current_right;
+                left = curr_left;
+                right = curr_right;
             }
-
-
         }
         output.push_back({left, right});
+
         return output;
     }
 };
