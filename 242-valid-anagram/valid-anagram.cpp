2fb8ecb319afddaaa9b995e158af85b0c377076b
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-05-01 19:20:16 +0530

    Time: 2 ms (58.85%) | Memory: 9.7 MB (48.43%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+
+        vector<int> s_hash(26, 0);
+        vector<int> t_hash(26, 0);
+
+        for(auto x: s){
+            s_hash[x-'a']++;
+        }
+
+        for(auto x: t){
+            t_hash[x-'a']++;
+        }
+
+        for(int i=0; i<26; i++){
+            if(s_hash[i]!=t_hash[i]){
+                return false;
+            }
+        }
+        return true;
+
+
+    }
+};
