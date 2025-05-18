0e7bb63b3adcd6b242453517af9eb762adc3edea
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-05-18 15:04:09 +0530

    Time: 1758 ms (9.14%) | Memory: 370.5 MB (8.19%) - LeetSync

--- 3837-grid-teleportation-traversal/grid-teleportation-traversal.cpp
+++ 3837-grid-teleportation-traversal/grid-teleportation-traversal.cpp
@@ -1,63 +1,67 @@
 class Solution {
 public:
     int minMoves(vector<string>& matrix) {
+        vector<vector<pair<int, int>>> portal(26);
+
         int n = matrix.size();
         int m = matrix[0].size();
+        vector<bool> usedPortal(26, false);
 
-        vector<vector<pair<int, int>>> portal(26);
-        for(int i = 0; i < n; ++i){
-            for(int j = 0; j < m; ++j){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 char ch = matrix[i][j];
-                if(ch >= 'A' && ch <= 'Z'){
-                    portal[ch - 'A'].emplace_back(i, j);
+                if(ch>='A' && ch<='Z'){
+                    portal[ch-'A'].push_back({i, j});
                 }
             }
         }
 
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
-        vector<bool> usedPortal(26, false);
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        dist[0][0]= 0;
+        pq.push({0, 0, 0});
 
-        // Min-heap: {cost, x, y}
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
-        pq.emplace(0, 0, 0); // cost, row, col
-
-        vector<pair<int, int>> directions = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-
-        while (!pq.empty()) {
+        while(!pq.empty()){
             auto [cost, x, y] = pq.top();
             pq.pop();
 
-            if (x < 0 || x >= n || y < 0 || y >= m || visited[x][y] || matrix[x][y] == '#')
-                continue;
-
-            if (x == n - 1 && y == m - 1)
+            if (x == n - 1 && y == m - 1){
                 return cost;
+            }
 
-            visited[x][y] = true;
-
-            // Check portals
             char ch = matrix[x][y];
-            if (ch >= 'A' && ch <= 'Z') {
-                int idx = ch - 'A';
-                if (!usedPortal[idx]) {
-                    usedPortal[idx] = true;
-                    for (auto& [nx, ny] : portal[idx]) {
-                        if (!visited[nx][ny]) {
-                            pq.emplace(cost, nx, ny); // teleport: same cost
-                        }
+            // cout<<ch<<" ";
+
+
+
+
+
+            if(ch>='A' && ch<='Z' && !usedPortal[ch-'A']){
+                usedPortal[ch-'A'] = true;
+                for(auto [i, j]: portal[ch-'A']){
+                    if(dist[i][j]>cost){
+                        dist[i][j] = cost;
+                        pq.push({cost, i, j});
                     }
                 }
             }
 
-            // Check 4 directions
-            for (auto& [dx, dy] : directions) {
-                int nx = x + dx, ny = y + dy;
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visited[nx][ny]) {
-                    pq.emplace(cost + 1, nx, ny);
+            vector<pair<int, int>> dir({{0,1}, {1,0}, {-1, 0}, {0,-1}});
+            for(auto [dx, dy]: dir){
+                int xi = x+dx;
+                int yj = y+dy;
+                if(xi >= 0 && xi < n && yj >= 0 && yj < m && matrix[xi][yj] != '#'){
+                    if(dist[xi][yj]> cost+1){
+                        dist[xi][yj] = cost+1;
+                        pq.push({cost+1, xi, yj});
+                    }
                 }
             }
         }
+        return -1;
 
-        return -1; // if unreachable
+        
+        
     }
+
 };
