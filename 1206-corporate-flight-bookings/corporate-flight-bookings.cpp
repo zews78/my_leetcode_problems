2ef13e49d8aa7a54959e43af2ba3da3b6a52109b
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-05-23 19:42:15 +0530

    Time: 0 ms (100.00%) | Memory: 72.8 MB (45.46%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
@@ -2,13 +2,19 @@ class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
         vector<int> ans(n, 0);
+        vector<int> diff(n+1, 0);
         for(int i=0; i<bookings.size(); i++){
             int st = bookings[i][0];
             int end = bookings[i][1];
             int val = bookings[i][2];
-            for(int j=st; j<=end; j++){
-                ans[j-1]+=val;
-            }
+            diff[st-1]+=val;
+            diff[end]-=val;
+        }
+
+        int seats =0;
+        for(int i=0; i<n; i++){
+            seats += diff[i];
+            ans[i] = seats;
         }
         return ans;
     }
