39226b4d1a00816725160f9efeabc12d15e23ef3
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-06-01 14:12:24 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (46.47%) - LeetSync

--- 3843-partition-array-into-two-equal-product-subsets/partition-array-into-two-equal-product-subsets.cpp
+++ 3843-partition-array-into-two-equal-product-subsets/partition-array-into-two-equal-product-subsets.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
-    void solve(int n, vector<int>& nums, long long target, unordered_set<int>& set, bool& target_found){
-        if(n==0){
-            if(target==1){
-                target_found = true;
-            }
-            return;
-        }
-        if(nums[n-1]<=target && target%nums[n-1]==0){
-            set.insert(nums[n-1]);
-            solve(n-1, nums, target/nums[n-1], set, target_found);
-            if(target_found) return;
-            set.erase(nums[n-1]);
-        }
-        solve(n-1, nums, target, set, target_found);
-    }
+    typedef __int128 ll;
+    // void solve(int n, vector<int>& nums, long long target, unordered_set<int>& set, bool& target_found){
+    //     if(n==0){
+    //         if(target==1){
+    //             target_found = true;
+    //         }
+    //         return;
+    //     }
+    //     if(nums[n-1]<=target && target%nums[n-1]==0){
+    //         set.insert(nums[n-1]);
+    //         solve(n-1, nums, target/nums[n-1], set, target_found);
+            
+    //         set.erase(nums[n-1]);
+    //     }
+    //     solve(n-1, nums, target, set, target_found);
+    // }
     bool checkEqualPartitions(vector<int>& nums, long long target) {
-        unordered_set<int> set;
-        bool target_found = false;
-        solve(nums.size(), nums, target, set, target_found);
-        if(set.size()==nums.size() || set.size()==0){
-            return false;
+        int n = nums.size();
+        ll total = 1;
+        for (int x : nums) {
+            total *= x;
+            if (total > (ll)target * target) return false;
         }
-        // for(auto x: set){
-        //     cout<<x<<" ";
-        // }
-        long long product2=1;
-        for(auto x: nums){
-            if(!set.count(x)){
-                product2*= x;
-            }
-        }
-        if(product2 == target){
-            return true;
-        }
-        return false;
+
+        if (total != (ll)target * target) return false;
+        return true;
     }
 };
