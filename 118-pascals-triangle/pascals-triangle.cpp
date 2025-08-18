5aba44ce7b906fb97b9dcd187bdc80d0fe9e5ffe
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-08-18 11:35:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (32.25%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> output;
-        output.push_back({1});
-        for(int i=0; i<numRows-1; i++){
-            vector<int> temp;
-            int lastIndex = output.size()-1;
-            cout<<lastIndex<<"-"<<output[lastIndex].size()<<endl;
-            temp.push_back(1);
-            for(int j=1; j<output[lastIndex].size(); j++){
-                int last_sum = output[lastIndex][j]+output[lastIndex][j-1];
-                cout<<last_sum<<" ";
-                temp.push_back(last_sum);
+        vector<vector<int>> output(numRows);
+        for(int i=0; i<numRows; i++){
+            output[i].resize(i+1);
+            for(int j=0; j<=i; j++){
+                if(j==0 || j==i){
+                    output[i][j] = 1;
+                }else{
+                    output[i][j] = output[i-1][j] + output[i-1][j-1];
+                }
             }
-            temp.push_back(1);
-            output.push_back(temp);
-            temp = {};
         }
-
         return output;
     }
 };
