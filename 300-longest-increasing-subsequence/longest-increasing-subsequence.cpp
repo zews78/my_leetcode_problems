fdf249e0457bfdaebcb791a95068ad869146c0c2
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-11-08 23:04:36 +0530

    Time: 643 ms (5.04%) | Memory: 296.8 MB (6.34%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int getSubsequences(int i, int prevIndex, vector<int>& nums, vector<vector<int>>& dp){
-        if(i == nums.size()) return 0;
-
-        if(dp[i][prevIndex + 1] != -1) return dp[i][prevIndex + 1];
-
-        int take = 0;
-        if(prevIndex == -1 || nums[i] > nums[prevIndex]){
-            take = 1 + getSubsequences(i + 1, i, nums, dp);
+    int lengthOfLIS(vector<int>& nums) {
+        int prev = -1;
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return solve(0, prev, nums, dp);
+    }
+    int solve(int i, int prev, vector<int>& nums, vector<vector<int>>& dp){
+        if(i==nums.size()){
+            return 0;
+        }
+        if(dp[i][prev+1]!=-1){
+            return dp[i][prev+1];
         }
 
-        int notTake = getSubsequences(i + 1, prevIndex, nums, dp);
-
-        return dp[i][prevIndex + 1] = max(take, notTake);
-    }
-
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        // prevIndex goes from -1 to n-1 => dp[i][prev+1]
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return getSubsequences(0, -1, nums, dp);
+        if(prev==-1 || (prev!=-1 && nums[i]>nums[prev])){
+            return dp[i][prev+1] = max(1+solve(i+1, i, nums, dp), solve(i+1, prev, nums,dp));
+        }else{
+            return dp[i][prev+1] = solve(i+1, prev, nums, dp);
+        }
     }
 };
