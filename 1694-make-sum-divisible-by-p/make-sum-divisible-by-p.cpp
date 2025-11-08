91b16a88777c53d2d68974176d90b254adc911b2
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-11-09 01:40:27 +0530

    Time: 62 ms (96.63%) | Memory: 84.3 MB (95.73%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-int minSubarray(vector<int>& nums, int p) {
-    long long totalSum = 0;
-    for(int x : nums) totalSum += x;
-    
-    int rem = totalSum % p;
-    if(rem == 0) return 0; // already divisible
-    
-    unordered_map<int,int> mp;
-    mp[0] = -1; // prefix before array starts
-    
-    long long prefix = 0;
-    int ans = nums.size();
-    
-    for(int i = 0; i < nums.size(); i++){
-        prefix = (prefix + nums[i]) % p;
-        int needed = (prefix - rem + p) % p;
-        
-        if(mp.count(needed)){
-            ans = min(ans, i - mp[needed]);
+    int minSubarray(vector<int>& nums, int p) {
+        int n = nums.size();
+        long long total_sum =0;
+        for(auto x: nums){
+            total_sum+=x;
         }
-        
-        mp[prefix] = i; // store/update latest prefix index
+        if(total_sum<p) return -1;
+
+        int rem = total_sum%p;
+        if(rem==0) return 0;
+
+        unordered_map<long long,int> um;
+        um[0] = -1;
+        long long prefix=0;
+        int min_len = INT_MAX;
+
+        for(int i=0; i<n; i++){
+            prefix = (prefix + nums[i])%p;
+            int need = (prefix-rem+p)%p;
+            if(um.count(need)){
+                min_len = min(min_len, i-um[need]);
+            }
+            um[prefix] =i;
+        }
+        return (min_len==INT_MAX || min_len ==n)? -1: min_len;
     }
-    
-    return (ans == nums.size() ? -1 : ans);
-}
 };
