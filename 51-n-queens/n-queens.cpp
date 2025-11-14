298d41b7c68707234bf3b9d3b54a68f282fe34ca
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-11-14 20:52:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (92.22%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,53 +1,59 @@
 class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
-        // vector<vector<stirng>> board(n, vector<string>(".", n));
-        vector<vector<string>> mat;
-        vector<string> board;
-        string temp(n, '.');
-
-        for(int i=0; i<n; i++){
-            board.push_back(temp);
-        }
-        solve(0, n, board, mat);
-        return mat;
+        vector<string> board(n, string(n, '.'));
+        vector<vector<string>> res;
+        solve(0, n, board, res);
+        return res;
     }
 
-    void solve(int i, int n, vector<string> &board, vector<vector<string>> &mat){
+    void solve(int i, int n, vector<string> &board, vector<vector<string>> &res){
         if(i==n){
-            mat.push_back(board);
+            res.push_back(board);
             return;
         }
 
         for(int j=0; j<n; j++){
-            if(checkPossible(i, j, board, n)){
-                board[i][j] = 'Q';
-                solve(i+1, n, board, mat);
-                board[i][j] = '.';
+            if(feasible(i, j, board)){
+                board[i][j]='Q';
+                solve(i+1, n, board, res);
+                board[i][j]='.';
             }
         }
     }
 
-    bool checkPossible(int i, int j, vector<string> &board, int n){
-        int i1 =i, j1=j;
-        while(i1>=0 && j1>=0){
-            if(board[i1][j1]=='Q') return false;
-            i1--;j1--;
+    bool feasible(int i, int j, vector<string> &board){
+        // return true;
+        int n=board.size();
+        int x=i;
+        int y=j;
+        while(x>=0){
+            if(board[x][y]=='Q'){
+                return false;
+            }
+            x--;
         }
 
-        int i2=i;
-        while(i2>=0){
-            if(board[i2][j]=='Q') return false;
-            i2--;
+        x=i;
+        y=j;
+        while(x>=0 && y>=0){
+            if(board[x][y]=='Q'){
+                return false;
+            }
+            x--;
+            y--;
         }
 
-        int i3=i, j3=j;
-        while(i3>=0 && j3<n){
-            if(board[i3][j3]=='Q') return false;
-            i3--;j3++;
+        x=i;
+        y=j;
+        while(x>=0 && y<n){
+            if(board[x][y]=='Q'){
+                return false;
+            }
+            x--;
+            y++;
         }
 
         return true;
     }
-    
 };
