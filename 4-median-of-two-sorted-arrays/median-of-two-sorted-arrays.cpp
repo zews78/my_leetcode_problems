c6c928012a83d31ebeb00f0aadc3312f33adfc4a
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-11-16 18:16:45 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (46.30%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -4,42 +4,47 @@ public:
         int n1 = nums1.size();
         int n2 = nums2.size();
 
-        if(n1>n2){
+        if(n2<n1){
             return findMedianSortedArrays(nums2, nums1);
         }
 
-        int n = n1+n2;
-        int s =0, e = n1;
+        int n=n1+n2;
+    
+        int st=0;
+        int e=n1;
 
-        while(s<=e){
-            int mid1 = s+(e-s)/2;
-            int mid2 = ((n+1)/2) - mid1;
+        while(st<=e){
+            int mid1 = st+(e-st)/2;
+            int mid2 = ((n+1)/2)-mid1;
+            // int mid2 = n2-mid1-1;
 
-            int l1 = INT_MIN, l2 = INT_MIN;
-            int r1 = INT_MAX, r2 = INT_MAX;
+            int l1 = INT_MIN;
+            int l2 = INT_MIN;
+            int r1 = INT_MAX;
+            int r2 = INT_MAX;
 
             if(mid1-1>=0) l1 = nums1[mid1-1];
             if(mid2-1>=0) l2 = nums2[mid2-1];
 
-            if(mid1<n1) r1= nums1[mid1];
-            if(mid2<n2) r2= nums2[mid2];
+            if(mid1<n1) r1 = nums1[mid1];
+            if(mid2<n2) r2 = nums2[mid2];
 
             if(l1<=r2 && l2<=r1){
                 if(n%2==0){
                     return (max(l1, l2) + min(r1, r2))/2.0;
-                }else{
+                }
+                else{
                     return max(l1, l2);
                 }
             }
-            //edge cases
-            if(l1>r2){
+            else if(l1>r2){
                 e = mid1-1;
             }else{
-                s = mid1+1;
+                st = mid1+1;
             }
+
         }
+
         return 0;
-
-
     }
 };
