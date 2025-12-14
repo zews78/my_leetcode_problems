439dfbaceb4a658cc18b6112775dcf611ed1714e
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-12-14 13:27:04 +0530

    Time: 35 ms (5.64%) | Memory: 9.7 MB (99.88%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,37 +1,51 @@
 class Solution {
 public:
-vector<string> splitBySpace(const string &s) {
-    vector<string> result;
-    string temp;
+    string reverseWords(string s) {
+    int n=s.size();
+    reverse(s.begin(), s.end());
+    int i=0;
+    int j=0;
+    int curr =0;
+    while(j<n){
+        if(s[curr]==' '){
+            curr++; j++;
+        }else{
+            while(j<n && s[j]!=' '){
+                j++;
+            }
 
-    for (char c : s) {
-        if (c == ' ') {
-            if (!temp.empty())
-                result.push_back(temp);
-            temp.clear();
-        } else {
-            temp += c;
+            int word_len = j-curr;
+            reverse_word(s, curr, j-1);
+            cout<<i<<curr<<j<<endl;
+            while(word_len){
+                s[i]=s[curr];
+                if(i!=curr) s[curr]=' ';
+                i++; curr++;
+                word_len--;
+            }
+            s[i]=' ';
+            i++;
+
+            curr= i;
+            j=i;
         }
+
+
     }
 
-    if (!temp.empty())
-        result.push_back(temp);
+    s.resize(i-1);
 
-    return result;
-}
-    string reverseWords(string s) {
-        int n= s.size();
-        vector<string> words = splitBySpace(s);
-        
-        string res = "";
-        for(int i=words.size()-1; i>=0; i--){
-            res+=words[i];
-            if(i!=0){
-                res+=" ";
-            }
+    return s;
+    }
+
+
+    void reverse_word(string &s, int st, int end){
+        while(st<end){
+            swap(s[st], s[end]);
+            st++; end--;
         }
-
-        return res;
-        
     }
 };
+
+
+
