c431495087e766f0bc9d194aa1c3391cd5e9574a
Sharique Alam <56071706+zews78@users.noreply.github.com>
2025-12-17 23:06:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (10.30%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,19 +1,52 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int h = haystack.size();
         int n = needle.size();
-        for(int i=0; i<h; i++){
-            int curr = i;
-            int ndl_idx = 0;
-            while(curr<h && ndl_idx<n && haystack[curr]==needle[ndl_idx]){
-                curr++;
-                ndl_idx++;
+        int h = haystack.size();
+        if(n==0) return 0;
+        if(n>h) return -1;
+
+        //construct lps(longest prefix suffix) for needle
+        // the significance of lps is it tracks the repeated chars!
+        int prev=0;
+        int i=1;
+        vector<int> lps(n, 0);
+        while(i<n){
+            if(needle[i]==needle[prev]){
+                lps[i] = prev+1;
+                i++;
+                prev++;
             }
-            if(ndl_idx == n){
-                return i;
+            else if(prev==0){
+                lps[i] =0;
+                i++; //imp
+            }else{
+                prev = lps[prev-1];
             }
         }
+
+
+        // iterate over haystack
+        //track back on needle if needed
+
+        i=0;
+        int j=0;
+        while(i<h){
+            if(haystack[i]==needle[j]){
+                i++; j++;
+            }else{
+                if(j>0){
+                    j = lps[j-1];
+                }else{
+                    j=0;
+                    i++;
+                }
+            }
+            if(j==n){
+                return (i-n);
+            }
+        }
+
         return -1;
     }
 };
