59a4295702e3aa835e9863c65a9655ba9cebae2c
Sharique Alam <56071706+zews78@users.noreply.github.com>
2026-01-10 12:26:42 +0530

    Time: 3 ms (18.74%) | Memory: 17.1 MB (6.43%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,20 +11,37 @@
  */
 class Solution {
 public:
+    vector<int> next_greater_ele;
+    void cal_next_greater_ele(vector<int>& preorder){
+        int n= preorder.size();
+        stack<pair<int, int>> st;
+        st.push({INT_MAX, n});
+
+        for(int i=n-1; i>=0; i--){
+            int curr = preorder[i];
+            while(!st.empty() && st.top().first<curr){
+                st.pop();
+            }
+            next_greater_ele[i] = st.top().second;
+            st.push({curr, i});
+        }
+    }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
         int st = 0;
         int end = preorder.size()-1;
+        next_greater_ele.resize(preorder.size());
+        cal_next_greater_ele(preorder);
         return solve(st, end, preorder);
     }
-    int getNGE(int st, int end, vector<int> &preorder){
-        int val = preorder[st];
-        st++;
-        while(st<=end && val>preorder[st]){
-            st++;
-        }
+    // int getNGE(int st, int end, vector<int> &preorder){
+    //     int val = preorder[st];
+    //     st++;
+    //     while(st<=end && val>preorder[st]){
+    //         st++;
+    //     }
 
-        return st;
-    }
+    //     return st;
+    // }
     TreeNode* solve(int st, int end, vector<int>& preorder){
         if(st>end){
             return nullptr;
@@ -32,7 +49,7 @@ public:
         int val = preorder[st];
         TreeNode* node = new TreeNode(val);
 
-        int nge = getNGE(st, end, preorder);
+        int nge = next_greater_ele[st];
         // cout<<nge<<endl;
 
         node->left = solve(st+1, nge-1, preorder);
