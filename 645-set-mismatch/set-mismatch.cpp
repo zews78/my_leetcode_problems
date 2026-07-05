428d4bc2e24cdfb2350cfe6025a6ca24d69415fe
Sharique Alam <56071706+zews78@users.noreply.github.com>
2026-07-05 23:14:14 +0530

    Time: 0 ms (100.00%) | Memory: 25.2 MB (63.55%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+
+        int i=0;
+        while(i<n){
+            int curr = nums[i];
+            if(curr!=nums[curr-1]){
+                swap(nums[i], nums[curr-1]);
+            }else{
+                i++;
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            if(nums[i]!=i+1){
+                return {nums[i], i+1};
+            }
+        }
+        return {};
+    }
+};
