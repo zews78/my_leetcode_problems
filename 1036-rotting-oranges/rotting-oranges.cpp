41564174838cb5286fa655108f29bb2761f9875d
Sharique Alam <56071706+zews78@users.noreply.github.com>
2026-08-02 17:21:52 +0530

    Time: 4 ms (13.68%) | Memory: 18.2 MB (5.09%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,44 +3,48 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int min_ans = -1;
-        int count_min = 0;
 
-        // if(grid == [[0]])
-        bool isEmpty = true;
-        queue<pair<int, int>> q;
+
+
+        queue<tuple<int, int, int>> q;
+
+        vector<vector<int>> dirs({
+            {0,1}, {0, -1},
+            {-1, 0}, {1, 0}
+        });
+
+        int max_time = 0;
+
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(grid[i][j]==1 || grid[i][j]==2){
-                    isEmpty = false;
-                }
                 if(grid[i][j]==2){
-                    q.push({i, j});
+                    q.push({i, j, 0});
                 }
             }
         }
-
-        if(isEmpty) return 0;
 
         while(!q.empty()){
-            int q_len = q.size();
+            int q_size = q.size();
 
-            for(int i=0; i<q_len; i++){
-                auto front = q.front();
+            for(int i=0; i<q_size; i++){
+                auto [x, y, time] = q.front();
                 q.pop();
-                //ULDR
-                vector<vector<int>> dir({{-1, 0}, {0, -1}, {1, 0}, {0, 1}});
-                for(auto x: dir){
-                    int dx = front.first + x[0];
-                    int dy = front.second + x[1];
-                    if(dx>=0 && dy>=0 && dx<n && dy<m && grid[dx][dy]==1){
-                        grid[dx][dy] = 2;
-                        q.push({dx, dy});
+
+                for(auto dir: dirs){
+                    int dx = x+dir[0];
+                    int dy = y+dir[1];
+
+                    if(dx>=0 && dx<n && dy>=0 && dy<m && grid[dx][dy]==1){
+                        grid[dx][dy]=2;
+                        q.push({dx,dy,time+1});
+                        max_time = max(max_time, time+1);
                     }
                 }
+
             }
-            count_min++;
+
         }
+
 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
@@ -50,7 +54,9 @@ public:
             }
         }
 
-        return count_min-1;
+        return max_time;
+
+
         
     }
 };
