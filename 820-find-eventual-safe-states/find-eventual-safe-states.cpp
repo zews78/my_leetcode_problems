1a15936e55d7c40077118c289cfd25c3d767583e
Sharique Alam <56071706+zews78@users.noreply.github.com>
2026-08-07 00:07:43 +0530

    Time: 50 ms (43.25%) | Memory: 66.1 MB (52.71%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        //kahn's algo using BFS
         int V = graph.size();
-        vector<int> state(V, 0);
-        vector<int> safe_state;
+        vector<int> outdegree(V, 0);//dobara reverse ka indegree nikalne se bach gye
+
+        vector<vector<int>> rev(V);
 
         for(int i=0; i<V; i++){
-            if(dfs(i, V, graph, state)){
-                safe_state.push_back(i);
+            outdegree[i]+=graph[i].size();
+            for(auto x: graph[i]){
+                rev[x].push_back(i);
             }
         }
 
+        queue<int> q;
+
+        for(int i=0; i<V; i++){
+            if(outdegree[i]==0){
+                q.push(i);
+            }
+        }
+
+        vector<int> safe_state;
+
+        while(!q.empty()){
+            auto front = q.front();
+            q.pop();
+            
+            safe_state.push_back(front);
+
+            for(auto x: rev[front]){
+                outdegree[x]--;
+                if(outdegree[x]==0){
+                    q.push(x);
+                }
+            }
+        }
+
+        sort(safe_state.begin(), safe_state.end());
         return safe_state;
-
-    }
-
-
-    bool dfs(int i, int V, vector<vector<int>>& graph, vector<int> &state){
-        if(state[i]==1){
-            return false;
-        }
-
-        if(state[i]==2){
-            return true;
-        }
-
-        state[i]=1;
-
-        for(auto x: graph[i]){
-            if(!dfs(x, V, graph, state)){
-                return false;
-            }
-        }
-
-        state[i]=2;
-        return true;
     }
 };
