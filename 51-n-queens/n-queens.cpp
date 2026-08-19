30212f2cc917aa2955f272246280d60ad866d3aa
Sharique Alam <56071706+zews78@users.noreply.github.com>
2026-08-19 20:43:44 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (59.42%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,59 +1,45 @@
 class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> board(n, string(n, '.'));
         vector<vector<string>> res;
-        solve(0, n, board, res);
+        vector<string> board(n, string(n, '.'));
+
+        vector<bool> col_vis(n, false);
+        vector<bool> d1_vis(2*n-1, false);
+        vector<bool> d2_vis(2*n-1, false);
+
+        solve(0, 0, n, col_vis, d1_vis, d2_vis, res, board);
+
         return res;
+
     }
 
-    void solve(int i, int n, vector<string> &board, vector<vector<string>> &res){
+    void solve(int i, int j, int n, vector<bool> &col_vis, vector<bool> &d1_vis, vector<bool> &d2_vis, vector<vector<string>> &res, vector<string> &board){
         if(i==n){
             res.push_back(board);
-            return;
         }
 
         for(int j=0; j<n; j++){
-            if(feasible(i, j, board)){
-                board[i][j]='Q';
-                solve(i+1, n, board, res);
-                board[i][j]='.';
+            if(col_vis[j] || d1_vis[i+j] || d2_vis[(n-1)+i-j]){
+                continue;
             }
+            board[i][j]='Q';
+            col_vis[j]=true;
+            d1_vis[i+j]=true;
+            d2_vis[(n-1)+i-j]=true;
+            solve(i+1, j, n, col_vis, d1_vis, d2_vis, res, board);
+            board[i][j]='.';
+            col_vis[j]=false;
+            d1_vis[i+j]=false;
+            d2_vis[(n-1)+i-j]=false;
         }
     }
 
-    bool feasible(int i, int j, vector<string> &board){
-        // return true;
-        int n=board.size();
-        int x=i;
-        int y=j;
-        while(x>=0){
-            if(board[x][y]=='Q'){
-                return false;
-            }
-            x--;
-        }
+    // bool checkFeasible(int i, int col, int n, vector<string> &board){
+    //     for(int i=0; i<col; i++){
 
-        x=i;
-        y=j;
-        while(x>=0 && y>=0){
-            if(board[x][y]=='Q'){
-                return false;
-            }
-            x--;
-            y--;
-        }
+    //     }
+    // }
 
-        x=i;
-        y=j;
-        while(x>=0 && y<n){
-            if(board[x][y]=='Q'){
-                return false;
-            }
-            x--;
-            y++;
-        }
 
-        return true;
-    }
 };
