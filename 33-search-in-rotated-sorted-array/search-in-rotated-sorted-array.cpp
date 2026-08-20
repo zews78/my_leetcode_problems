5522b6e113722305005220733b0d5137b53ab8fb
Sharique Alam <56071706+zews78@users.noreply.github.com>
2026-08-21 00:26:16 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (36.77%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -2,51 +2,35 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n=nums.size();
+
         int st=0;
-        int end = n-1;
-        int ans=0;
+        int end=n-1;
+        // int v0 = nums[0]; not needed
 
         while(st<=end){
             int mid = st+(end-st)/2;
-            if(nums[mid]<nums[0]){
-                ans =mid;
-                end = mid-1;
-            }else{
-                st= mid+1;
-            }
-        }
-
-        st=0;
-        end=ans-1;
-        while(st<=end){
-            int mid = st+(end-st)/2;
+            
             if(nums[mid]==target){
                 return mid;
             }
-            else if(nums[mid]>target){
-                end = mid-1;
+            else if(nums[st]<=nums[mid]){
+                //bs(st, mid)
+                if(target>=nums[st] && target<nums[mid]){
+                    end = mid-1;
+                }else{
+                    st = mid+1;
+                }
+                
+
             }else{
-                st = mid+1;
+                if(target>nums[mid] && target<=nums[end]){
+                    st = mid+1;
+                }else{
+                    end = mid-1;
+                }
             }
+
         }
-
-        st=ans;
-        end=n-1;
-        while(st<=end){
-            int mid = st+(end-st)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]>target){
-                end = mid-1;
-            }else{
-                st = mid+1;
-            }
-        }
-
-
         return -1;
     }
-
-
 };
