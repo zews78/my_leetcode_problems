c5c46009b4f93a3513979da0c7124cd3f6a9397b
Sharique Alam <56071706+zews78@users.noreply.github.com>
2026-08-29 18:55:58 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (61.29%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,50 +1,43 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
+        int prev=-1;
+        int curr=-1;
 
-        if(n2<n1){
-            return findMedianSortedArrays(nums2, nums1);
-        }
+        int n1=nums1.size();
+        int n2=nums2.size();
 
         int n=n1+n2;
-    
-        int st=0;
-        int e=n1;
+        int half = n/2;
 
-        while(st<=e){
-            int mid1 = st+(e-st)/2;
-            int mid2 = ((n+1)/2)-mid1;
-            // int mid2 = n2-mid1-1;
+        int i=0, j=0, k=0;
 
-            int l1 = INT_MIN;
-            int l2 = INT_MIN;
-            int r1 = INT_MAX;
-            int r2 = INT_MAX;
-
-            if(mid1-1>=0) l1 = nums1[mid1-1];
-            if(mid2-1>=0) l2 = nums2[mid2-1];
-
-            if(mid1<n1) r1 = nums1[mid1];
-            if(mid2<n2) r2 = nums2[mid2];
-
-            if(l1<=r2 && l2<=r1){
-                if(n%2==0){
-                    return (max(l1, l2) + min(r1, r2))/2.0;
-                }
-                else{
-                    return max(l1, l2);
-                }
-            }
-            else if(l1>r2){
-                e = mid1-1;
+        while(k<=half && i<n1 && j<n2){
+            prev = curr;
+            if(nums1[i]<=nums2[j]){
+                curr= nums1[i];
+                i++;
             }else{
-                st = mid1+1;
+                curr = nums2[j];
+                j++;
             }
-
+            k++;
+        }
+        while(k<=half && i<n1){
+            prev = curr;
+            curr= nums1[i];
+            i++; k++;
+        }
+        while(k<=half && j<n2){
+            prev = curr;
+            curr = nums2[j];
+            j++; k++;
         }
 
-        return 0;
+
+        if(n%2==0){
+            return (prev+curr)/2.0;
+        }
+        return curr;
     }
 };
