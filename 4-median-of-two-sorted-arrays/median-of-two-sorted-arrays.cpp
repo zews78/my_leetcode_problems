1dd29bf554e9262f2e5e486e7f2d5a17211f84d7
Sharique Alam <56071706+zews78@users.noreply.github.com>
2026-08-30 12:46:43 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (61.32%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int prev=-1;
-        int curr=-1;
-
         int n1=nums1.size();
         int n2=nums2.size();
+
+        // if(n1<n2){
+        //     return findMedianSortedArrays(nums2, nums1);
+        // }
+
+        int curr=-1;
+        int prev=-1;
 
         int n=n1+n2;
         int half = n/2;
 
-        int i=0, j=0, k=0;
+        int i=0;
+        int j=0;
 
-        while(k<=half && i<n1 && j<n2){
+        for(int k=0; k<=half; k++){
             prev = curr;
-            if(nums1[i]<=nums2[j]){
-                curr= nums1[i];
-                i++;
+            if(i >= n1 || (j<n2) && (nums1[i]>nums2[j])){
+                curr=nums2[j++];
             }else{
-                curr = nums2[j];
-                j++;
+                curr=nums1[i++];
             }
-            k++;
         }
-        while(k<=half && i<n1){
-            prev = curr;
-            curr= nums1[i];
-            i++; k++;
-        }
-        while(k<=half && j<n2){
-            prev = curr;
-            curr = nums2[j];
-            j++; k++;
-        }
+
 
 
         if(n%2==0){
