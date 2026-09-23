995e807781c073fe38426576d6e5b578955b8857
Sharique Alam <56071706+zews78@users.noreply.github.com>
2026-09-23 22:59:47 +0530

    Time: 28 ms (38.81%) | Memory: 153.8 MB (9.62%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
-        deque<pair<int, int>> dq;
-        vector<int> ans;
-
+        int n=nums.size();
+        deque<int> dq;
+        vector<int> res;
         int j=0;
         while(j<k){
-            while(!dq.empty() && dq.front().first < nums[j]){
+            while(!dq.empty() && nums[dq.front()]<=nums[j]){
                 dq.pop_front();
             }
-            dq.push_front({nums[j], j});
+            dq.push_front(j);
             j++;
         }
-        ans.push_back(dq.back().first);
+        res.push_back(nums[dq.back()]);
+
+
         int i=0;
         while(j<n){
-            //pop start
-            if(dq.back().second <= i){
+            if(!dq.empty() && dq.back()<=i){
                 dq.pop_back();
             }
 
-            //push in monotonic fashion
-            while(!dq.empty() && dq.front().first < nums[j]){
+            while(!dq.empty() && nums[dq.front()]<=nums[j]){
                 dq.pop_front();
             }
-            dq.push_front({nums[j], j});
-            ans.push_back(dq.back().first);
-            i++; j++;
+            dq.push_front(j);
+            res.push_back(nums[dq.back()]);
+            j++;
+            i++;
         }
 
-        return ans;
+        return res;
     }
 };
